9eb35b978a89deea60335346a593433485645638
Missicassie <Cassandre.ka@outlook.fr>
2023-12-08 01:52:31 +0100

    cauchemar v1

---
+++ button.js
@@ -0,0 +1,27 @@
+
+function moveQuestionRight() {
+    var questionDiv = document.querySelector(".question");
+    questionDiv.classList.add('moveRight');
+}
+
+
+function moveQuestionLeft() {
+    var questionDiv = document.querySelector(".question");
+    questionDiv.classList.add('moveLeft');
+}
+
+function cauchemar_button() {
+    var cauchemar_button = document.querySelector(".cauchemar");
+    cauchemar_button.classList.add('moveRandom');
+}
+
+let buttonV = document.querySelector(".oui");
+let buttonX = document.querySelector(".non");
+let buttonCauch = document.querySelector(".cauchemar");
+
+buttonV.addEventListener("click", moveQuestionRight);
+buttonX.addEventListener("click", moveQuestionLeft);
+buttonCauch.addEventListener("mouseenter",cauchemar_button);
+
+
+
