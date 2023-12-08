6a350f093eb40be5fc1cadfa1a6234c1f8232c5e
Cyril Pitar <98352804+AyRron@users.noreply.github.com>
2023-12-08 02:39:45 +0100

    vite

--- button.js
+++ button.js
@@ -1,13 +1,13 @@
 
 function moveQuestionRight() {
     var questionDiv = document.querySelector(".question");
-    questionDiv.classList.add('moveRight');
+    questionDiv.classList.add('moveLeft');
 }
 
 
 function moveQuestionLeft() {
     var questionDiv = document.querySelector(".question");
-    questionDiv.classList.add('moveLeft');
+    questionDiv.classList.add('moveRight');
 }
 
 function cauchemar_button() {
@@ -15,13 +15,26 @@ function cauchemar_button() {
     cauchemar_button.classList.add('moveRandom');
 }
 
+function get_random_position(){
+    let res = [ (Math.random() * 1920).toFixed(0) , (Math.random() * 1080).toFixed(0)]
+    return res;
+}
+
+function randomize_position(){
+    document.getElementById('button').style.left="".concat(get_random_position()[0]).concat("px");
+    console.log("".concat(get_random_position()[0]).concat("px"))
+    button.style.left = get_random_position()[0];
+    button.style.top = get_random_position()[1];
+}
+
+
 let buttonV = document.querySelector(".oui");
 let buttonX = document.querySelector(".non");
 let buttonCauch = document.querySelector(".cauchemar");
 
 buttonV.addEventListener("click", moveQuestionRight);
 buttonX.addEventListener("click", moveQuestionLeft);
-buttonCauch.addEventListener("mouseenter",cauchemar_button);
+buttonCauch.addEventListener("mouseenter",randomize_position);
 
 
 
